0b0503517679f109b29f38c021037f8f6259e33a
dwil2444 <daneandrew16@gmail.com>
2021-04-24 14:37:10 -0400

    clockwise selection

--- mousepoints.py
+++ mousepoints.py
@@ -15,27 +15,28 @@ def shadeRegion(coords, bound):
     print(resized.shape)
     top_left = coords[0]
     top_right = coords[1]
-    lower_left = coords[2]
-    lower_right = coords[3]
+    lower_left = coords[3]
+    lower_right = coords[2]
 
     color = ''
     if bound == "up":
-        color = (255 ,255 ,255)
+        color = (1 ,1 ,1)
         bound = 'ub'
     else:
         color = (0, 0 , 0)
         bound = 'lb'
 
+    # top left clockwise -> selection
     max_j  = lower_left[1]
     j = top_left[1]
     while j < max_j:
         for i in range(top_left[0], top_right[0]):
             resized[j][i] = color
         j += 1
-    filename = 'output/dave_small_' + bound + '.npy'
+    filename = 'generated_output/dave_small_' + bound + '.npy'
     x = np.moveaxis(resized, -1, 0)
     np.save(filename, x)
-    # cv.imshow("resized image", resized)
+    cv.imshow("resized image", resized)
 
 
 
@@ -48,17 +49,17 @@ def mousePoints(event, x, y, flags, params):
             coords.append((x, y))
         else:
             shadeRegion(coords=coords, bound='low')
-            shadeRegion(coords=coords, bound='up')
+            # shadeRegion(coords=coords, bound='up')
 
 
 
 def main():
     global resized
-    img = cv.imread('imgs/san_mateo.jpg')
+    img = cv.imread('imgs/san_mateo.jpg')/255
     resized = cv.resize(img, (100, 100))
     print(resized.shape)
     x = np.moveaxis(resized, -1, 0)
-    np.save('output/dave_small_orig_img.npy', x)
+    np.save('generated_output/dave_small_orig_img.npy', x)
     cv.imshow("resized image", resized)
     cv.setMouseCallback("resized image", mousePoints)
     cv.waitKey(0)
