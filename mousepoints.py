76848100ca2680fe7cd777abe98ab74d9dd2a5c1
dwil2444 <daneandrew16@gmail.com>
2021-04-24 20:23:59 -0400

    fixed ui, square shape and point selection

--- mousepoints.py
+++ mousepoints.py
@@ -12,42 +12,67 @@ coords = []
 global resized
 
 def shadeRegion(coords, bound):
+    global resized 
+
     print(coords)
     print(resized.shape)
     top_left = coords[0]
     top_right = coords[1]
-    lower_left = coords[3]
     lower_right = coords[2]
-
-    color = ''
+    lower_left = coords[3]
+    
+    color = 0
     if bound == "up":
-        color = (255 ,255 ,255)
+        color = 255
         bound = 'ub'
     else:
-        color = (0, 0 , 0)
+        color = 0
         bound = 'lb'
 
-    # top left clockwise -> selection
-    max_j  = lower_left[1]
-    j = top_left[1]
-    while j < max_j:
-        for i in range(top_left[0], top_right[0]):
-            resized[j][i] = color
-        j += 1
-    filename = 'generated_properties/dave_small_' + bound + '0.npy'
+    pts = np.array([top_left,top_right,lower_right,lower_left])
+    shape = cv.polylines(resized, [pts],True,(0, 0, 0))
+    cv.fillPoly(resized, [pts], color)
+    # cv.imshow("poly lines", resized)
+
     x = cv.cvtColor(resized, cv.COLOR_BGR2RGB)
+    filename = 'generated_properties/dave_small_' + bound + '0.npy'
     x = np.moveaxis(x, -1, 0)
     np.save(filename, x)
-    cv.imshow("resized image", resized)
+    cv.imshow("final image " + bound, resized)
+
+    # top left clockwise -> selection
+    # max_j  = lower_left[1]
+    # j = top_left[1]
+    # while j < max_j:
+    #     for i in range(top_left[0], top_right[0]):
+    #         resized[j][i] = color
+    #     j += 1
+    # filename = 'generated_properties/dave_small_' + bound + '0.npy'
+    # x = cv.cvtColor(resized, cv.COLOR_BGR2RGB)
+    # x = np.moveaxis(x, -1, 0)
+    # np.save(filename, x)
+    # cv.imshow("resized image", resized)
 
 
 
 def mousePoints(event, x, y, flags, params):
+    global counter
+    global coords
+    global resized
     if event == cv.EVENT_LBUTTONDOWN:
-        global counter
         counter += 1
-        global coords
         coords.append((x, y))
+        if counter == 2:
+            resized = cv.line(resized,coords[0],coords[1],(0,0,0),1)
+            cv.imshow("resized image", resized)
+        if counter == 3:
+            resized = cv.line(resized,coords[1],coords[2],(0,0,0),1)
+            cv.imshow("resized image", resized)
+        if counter == 4:
+            resized = cv.line(resized,coords[2],coords[3],(0,0,0),1)
+            resized = cv.line(resized,coords[3],coords[0],(0,0,0),1)
+            cv.imshow("resized image", resized)
+
 
 
 
